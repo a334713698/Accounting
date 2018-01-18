2f374a09c46242f0bca53e6af969d0637e2dc79f
洪冬介 <hongdongjie334@gmail.com>
2018-01-18 09:15:36 +0800

    ~~

--- Accounting/Main/Controller/Manage/HDJManageViewController.m
+++ Accounting/Main/Controller/Manage/HDJManageViewController.m
@@ -8,59 +8,99 @@
 
 #import "HDJManageViewController.h"
 
-@interface HDJManageViewController ()
+@interface HDJManageViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
 @implementation HDJManageViewController
 
-- (void)viewDidLoad {
+
+
+
+#pragma mark - lazy load
+- (UITableView *)tableView{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+        [self.view addSubview:_tableView];
+        _tableView.backgroundColor = BACKGROUND_COLOR;
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.showsVerticalScrollIndicator = NO;
+        _tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0.01, adaptHeight(10))];
+        if ([[[UIDevice currentDevice] systemVersion] doubleValue] > 11.0) {
+            _tableView.estimatedSectionHeaderHeight = 10;
+            _tableView.estimatedSectionFooterHeight = 0.01;
+        }
+        [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.mas_equalTo(0);
+        }];
+    }
+    return _tableView;
+}
+
+
+#pragma mark - view func
+- (void)viewDidLoad{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self initNav];
+    
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 5;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell* cell = [UITableViewCell new];
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    DLog(@"cell：%ld-%ld",indexPath.section,indexPath.row);
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return adaptHeight(44);
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return adaptHeight(12);
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 0.01;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    return nil;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section{
+    return nil;
+}
+
+#pragma mark - SEL
+
+
+#pragma mark - Method
+- (void)initNav{
     self.navTitle = @"理财";
     self.view.backgroundColor = WHITE_COLOR;
 
 }
 
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    //0.数据库文件路径
-    NSString* dbPath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingPathComponent:@"db_test.sqlite"];
-    DLog(@"%@",dbPath);
-    
-    //1.创建一个数据库文件
-    FMDatabase* database = [FMDatabase databaseWithPath:dbPath];
-    //打开数据库对象
-    BOOL isOpen = [database open];
-    
-    if (isOpen) {
-        //2.创建表（有主键）
-        BOOL isCreated = [database executeUpdate:@"create table if not exists Stu (id integer primary key,name text,age integer)"];
-        
-        if (isCreated) {
-            //3.插入操作
-            [database executeUpdate:@"insert into Stu (name,age) values ('Nancy',18)"];
-            
-            //4.查询操作
-            FMResultSet *result = [database executeQuery:@"select * from Stu"];
-            while([result next]) {
-                //根据给定字段取值
-                int ID = [result intForColumn:@"id"];
-                NSString* name = [result stringForColumn:@"name"];
-                int age = [result intForColumn:@"age"];
-                NSLog(@"%d,%@,%d",ID,name,age);
-            }
-            
-            
-        }else{
-            NSLog(@"表创建失败");
-        }
-        
-    }else{
-        NSLog(@"数据库打开失败");
-    }
-    
-    //5.关闭数据库
-    [database close];
-}
+
+
+#pragma mark - NetRequest
 
 @end
--- Accounting/Main/Controller/Property/HDJPropertyViewController.m
+++ Accounting/Main/Controller/Property/HDJPropertyViewController.m
@@ -8,97 +8,100 @@
 
 #import "HDJPropertyViewController.h"
 
-@interface HDJPropertyViewController ()
+@interface HDJPropertyViewController ()<UITableViewDelegate,UITableViewDataSource>
 
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
 @implementation HDJPropertyViewController
 
 
-- (void)viewDidLoad {
+#pragma mark - lazy load
+- (UITableView *)tableView{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+        [self.view addSubview:_tableView];
+        _tableView.backgroundColor = BACKGROUND_COLOR;
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.showsVerticalScrollIndicator = NO;
+        _tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0.01, adaptHeight(10))];
+        if ([[[UIDevice currentDevice] systemVersion] doubleValue] > 11.0) {
+            _tableView.estimatedSectionHeaderHeight = 10;
+            _tableView.estimatedSectionFooterHeight = 0.01;
+        }
+        [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.mas_equalTo(0);
+        }];
+    }
+    return _tableView;
+}
+
+
+#pragma mark - view func
+- (void)viewDidLoad{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self initNav];
+    
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 5;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    UITableViewCell* cell = [UITableViewCell new];
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    DLog(@"cell：%ld-%ld",indexPath.section,indexPath.row);
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return adaptHeight(44);
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return adaptHeight(12);
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 0.01;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    return nil;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section{
+    return nil;
+}
+
+#pragma mark - SEL
+
+
+#pragma mark - Method
+- (void)initNav{
     self.navTitle = @"资产";
     self.view.backgroundColor = WHITE_COLOR;
-    
-}
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    //打开数据库对象
-    BOOL isOpen = [self.dbMgr.database open];
-    
-    
-    if (isOpen) {
-        
-        /**
-         
-            //4.查询操作
-            FMResultSet *result = [self.dbMgr.database executeQuery:@"select * from Stu"];
-            while([result next]) {
-                //根据给定字段取值
-                int ID = [result intForColumn:@"id"];
-                NSString* name = [result stringForColumn:@"name"];
-                int age = [result intForColumn:@"age"];
-                NSLog(@"%d,%@,%d",ID,name,age);
-        */
-        
-        [self init_all_income_expenses_table];
-        [self init_inuse_income_expenses_table];
-        [self init_record_income_expenses_table];
-        [self init_project_type_table];
-        
-        [self.dbMgr getAllTuplesFromTabel:all_income_expenses_table];
-    }else{
-        NSLog(@"数据库打开失败");
-    }
-    
-    //5.关闭数据库
-    [self.dbMgr.database close];
-}
-
-//创建all_income_expenses_table，并插入数据
-- (void)init_all_income_expenses_table{
-    
-    BOOL isCreated = [self.dbMgr createTableWithName:all_income_expenses_table andKeyValues:@{@"cate_id":@"integer primary key",@"name":@"text",@"type_id":@"integer",@"icon":@"text"}];
-
-    if (isCreated) {
-        [self.dbMgr insertDataIntoTableWithName:all_income_expenses_table andKeyValues:@{@"type_id":@"1",@"icon":@"\'icon_star\'"}];
-        [self.dbMgr insertDataIntoTableWithName:all_income_expenses_table andKeyValues:@{@"type_id":@"2",@"icon":@"\'icon_star\'"}];
-    }
-}
-
-//创建inuse_income_expenses_table，并插入数据
-- (void)init_inuse_income_expenses_table{
-
-    BOOL isCreated = [self.dbMgr createTableWithName:inuse_income_expenses_table andKeyValues:@{@"cate_id":@"integer primary key",@"name":@"text",@"type_id":@"integer",@"icon":@"text"}];
-    
-    if (isCreated) {
-        
-    }
-}
-
-//创建record_income_expenses_table，并插入数据
-- (void)init_record_income_expenses_table{
-
-    BOOL isCreated = [self.dbMgr createTableWithName:record_income_expenses_table andKeyValues:@{@"record_id":@"integer primary key",@"name":@"text",@"type_id":@"integer",@"amount":@"double",@"create_time":@"integer",@"icon":@"text"}];
-
-    if (isCreated) {
-        
-    }
 
 }
 
-//创建project_type_table，并插入数据
-- (void)init_project_type_table{
-    
-    BOOL isCreated = [self.dbMgr createTableWithName:project_type_table andKeyValues:@{@"type_id":@"integer primary key",@"name":@"text"}];
-    
-    if (isCreated) {
-        [self.dbMgr insertDataIntoTableWithName:project_type_table andKeyValues:@{@"name":@"\'收入\'"}];
-        [self.dbMgr insertDataIntoTableWithName:project_type_table andKeyValues:@{@"name":@"\'支出\'"}];
-    }
 
-}
+
+#pragma mark - NetRequest
+
+
+
 
 @end
